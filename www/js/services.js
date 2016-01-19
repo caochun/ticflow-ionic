372afb5c72fed25ea43cb8d864c81e2997ac8ea6
lzl <nju10lzl@gmail.com>
2016-01-19 15:21:55 +0800

    prepare for distribute

--- www/js/services.js
+++ www/js/services.js
@@ -5,8 +5,8 @@ angular.module('ticflow.services', ['ticflow.utils'])
        var authenticatedKey = 'authenticated';
        var user = $localStorage.get(userKey);
 
-       //var base = "http://moon.nju.edu.cn:3000";
-       var base = "http://114.212.83.15:3000"; //lzl wired network @ room 812
+       var base = "http://moon.nju.edu.cn:3000";
+       //var base = "http://114.212.83.15:3000"; //lzl wired network @ room 812
        //var base = "http://localhost:3000";
 
        $rootScope.show = function (text) {
