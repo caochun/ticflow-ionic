e657e2f5862957fa3a66d1c8dd53e2ca324ff29e
lzl <nju10lzl@gmail.com>
2016-02-25 15:23:36 +0800

    update

--- www/js/controllers/NewListCtrl.js
+++ www/js/controllers/NewListCtrl.js
@@ -24,11 +24,6 @@ angular.module('ticflow.controllers')
         attached3: "",
     };
 
-    // $scope.units = [];
-    // $scope.names = [];
-    // $scope.addresses = [];
-    // $scope.phone_nos = [];
-
     $scope.$on('$ionicView.beforeEnter', function () {
         $scope.loadData();
     });
@@ -48,22 +43,6 @@ angular.module('ticflow.controllers')
             .error(function () {
                 $rootScope.notify("获取工程师列表失败！请检查您的网络！");
             });
-        // API.getClientInfo()
-        //     .success(function (lists) {
-        //         lists.forEach(function (entry) {
-        //             if ($scope.units.indexOf(entry.client.unit) == -1)
-        //                 $scope.units.push(entry.client.unit);
-        //             if ($scope.names.indexOf(entry.client.name) == -1)
-        //                 $scope.names.push(entry.client.name);
-        //             if ($scope.addresses.indexOf(entry.client.address) == -1)
-        //                 $scope.addresses.push(entry.client.address);
-        //             if ($scope.phone_nos.indexOf(entry.client.phone_no) == -1)
-        //                 $scope.phone_nos.push(entry.client.phone_no);
-        //         });
-        //     })
-        //     .error(function () {
-        //         $rootScope.notify("获取客户信息列表失败！请检查您的网络！");
-        //     });
     };
 
     $scope.newList = function() {
